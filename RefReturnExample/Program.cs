bd746d19e5fa9536f38163f03c1f66201c553104
Feroze basha S <bashaferoz66@gmail.com>
2024-07-31 12:31:04 +0530

    ref

--- RefReturnExample/Program.cs
+++ RefReturnExample/Program.cs
@@ -1,12 +1,44 @@
 ﻿
 
-Console.WriteLine("Hello, World!");
+
 
 class Student
 {
     //public field
     public int Grade = 2;
+
+    //public method
+    public void PrintGrade()
+    {
+        Console.WriteLine("Grade:"+Grade);
+    }
+
+    public ref int DoWork()
+    {
+        return ref Grade;
+    }
     
-    
+}
+
+class Program
+{
+    static void Main()
+    {
+        //creating object for student
+        var student = new Student();
+        
+        // call PrintGrade
+        student.PrintGrade();
+        
+        // call ref DoWork
+        ref int g = ref student.DoWork();
+        
+        //update the value of 'ref return'
+        g = 5;
+        // call PrintGrade after updating the value of 'ref return'
+        student.PrintGrade();
+        Console.ReadKey();
+
+    }
     
 }
