7d8abb427d40cea591c203198538051076fa16ce
Kali <bashaferoz027@gmail.com>
2024-08-02 09:20:55 +0530

    StaticClass

---
+++ StaticClassExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using StaticClassLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        // Calling the static class
+        Console.WriteLine(Country.CountryName);
+        Console.WriteLine(Country.Capital);
+        Console.WriteLine(Country.GetNoOfUnionTerritories());
+        
+    }
+}
---
+++ StaticClassLibrary/Country.cs
@@ -0,0 +1,13 @@
+namespace StaticClassLibrary;
+
+public static class Country
+{
+    public static string CountryName = "India";
+    public static string Capital = "New Delhi";
+
+    public static int GetNoOfUnionTerritories()
+    {
+        return 8;
+    }
+    
+}
